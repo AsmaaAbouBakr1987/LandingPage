da314754f783b9b780171840bd77fddaf314ecde
Asmaa Abou Bakr <asmaa.abobaker@yahoo.com>
2020-07-23 03:00:06 +0200

    edit active section function

--- js/app.js
+++ js/app.js
@@ -27,7 +27,18 @@ const sections = document.querySelectorAll('section');
 */
 
 
-
+const getActiveSection = () =>{
+    activeSection = sections[0];
+    let minVal = 150;
+    sections.forEach(section =>{
+        let bounding = section.getBoundingClientRect();
+        if (bounding.top > -150 && bounding.top < minVal) {
+            minVal = bounding.top;
+            activeSection = section;
+        };
+    });
+    return activeSection;
+};
 /**
  * End Helper Functions
  * Begin Main Functions
@@ -42,42 +53,40 @@ const navCreator = ()=>{
         const sectionID = section.id;
         const sectionDataNav = section.dataset.nav;
 
-        navContainer+= `<li><a class="menu__link" href="#${sectionID}">${sectionDataNav}</a></li>`
+        navContainer+= `<li class={${sectionID}}><a class="menu__link"  href="#${sectionID}">${sectionDataNav}</a></li>`
     })
     navigation.innerHTML= navContainer;
 }
 
-navCreator();
 
 // Add class 'active' to section when near top of viewport
 
-const offset = (section) =>{
-    return Math.floor(section.getBoundingClientRect().top);
-}
 
-const removeActiveClass = (section) =>{
-    section.classList.remove('your-active-class');
-}
 
-const addActiveClass = (conditional , section) =>{
-    if (conditional){
+const sectionActivation = () => {
+    window.addEventListener('scroll',  (event) => {
+        let section = getActiveSection();
         section.classList.add('your-active-class');
-    }
+        // set other sections as inactive
+
+        sections.forEach( item =>{
+            if (item.id != section.id && item.classList.contains('your-active-class')) {
+                item.classList.remove('your-active-class');
+            }
+        });
+
+        
+    });   
+       
+        
 }
 
-const sectionActivation = () =>{
-    sections.forEach(section => {
-        const elementOffside = offset(section);
 
-        inviewport = () => elementOffside < 150 && elementOffside >= -150 ;
-        removeActiveClass(section);
-        addActiveClass( inviewport(), section);
-    })
-}
 
-window.addEventListener( 'scroll', sectionActivation);
+
+
+
 // Scroll to anchor ID using scrollTO event
-
 
 /**
  * End Main Functions
@@ -87,8 +96,11 @@ window.addEventListener( 'scroll', sectionActivation);
 
 // Build menu 
 
+navCreator();
+
 // Scroll to section on link click
+
 
 // Set sections as active
 
-
+sectionActivation();
